6cea19805e05657b50a790689a8c2762b39d93ba
Bryan Castillo <bryanc@microsoft.com>
2019-06-04 22:38:11 -0700

    Fixing.

--- env-setup.bat
+++ env-setup.bat
@@ -1,4 +1,4 @@
-
+@echo off
 
 REM Deactivate a possible existing python virtual environment.
 CALL deactivate 2>nul
@@ -8,6 +8,17 @@ python -m venv venv
 
 CALL venv\Scripts\activate
 
+REM Install packages for jupyter
 python -m pip install jupyter jupyterlab pandas 
 
+REM Install packages for ELMo tutorial
+python -m pip install update spacy tensorflow tensorflow_hub sklearn xgboost spacy nltk
 
+REM download the spacy corpus for en
+python -m spacy download en
+
+REM download wordnet corpus
+python -c "import nltk; nltk.download('wordnet')"
+
+REM install dependencies for bert
+python pip install bert-tensorflow sklearn tensorflow_hub
